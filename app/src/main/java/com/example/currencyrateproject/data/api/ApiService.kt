f0d5c54825da127e96d7bb2370fdb42e3a5bdc15
ithirteeng <ithirteengw@gmail.com>
2022-09-27 21:57:45 +0700

    1) create some empty classes

---
+++ app/src/main/java/com/example/currencyrateproject/data/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.currencyrateproject.data.api
+
+class ApiService {
+}
---
+++ app/src/main/java/com/example/currencyrateproject/view/main_activity/cash_frgament/CashFragmentViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.currencyrateproject.view.main_activity.cash_frgament
+
+class CashFragmentViewModel {
+}
